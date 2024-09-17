741e3375ee49bad4b6ba3b89f3638fc75fa998f5
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-09-17 11:12:24 -0300

    Add trycatch

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -2,24 +2,28 @@ export function register() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js').then(async (serviceWorkerRegistration) => {
       alert('Service worker registered');
-      let subscription = await serviceWorkerRegistration.pushManager.getSubscription();
+      try {
+        let subscription = await serviceWorkerRegistration.pushManager.getSubscription();
 
-      if (!subscription) {
-        const response = await fetch(`${process.env.REACT_APP_API}/push/get-vapid-public-key`);
-        const vapidPublicKey = await response.text();
-        subscription = await serviceWorkerRegistration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: vapidPublicKey
+        if (!subscription) {
+          const response = await fetch(`${process.env.REACT_APP_API}/push/get-vapid-public-key`);
+          const vapidPublicKey = await response.text();
+          subscription = await serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: vapidPublicKey
+          });
+        }
+
+        await fetch(`${process.env.REACT_APP_API}/push/subscribe`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(subscription)
         });
+      } catch (error) {
+        alert('Error subscribing to push notifications:', error);
       }
-
-      await fetch(`${process.env.REACT_APP_API}/push/subscribe`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(subscription)
-      });
     }).catch((error) => {
       alert('Error registering service worker:', error);
     });
