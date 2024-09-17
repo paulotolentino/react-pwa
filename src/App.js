67f2162fb280c393081c6730a48ba824dc33890c
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-09-17 13:58:33 -0300

    Remove console.log and unnecessary button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
 import React from 'react';
-const registration = await navigator.serviceWorker.getRegistration();
 
 function App() {
-  const sendNotification = async () => {
-    if (Notification.permission === 'granted') {
-      showNotification("notifica");
-    }
-    else {
-      if (Notification.permission !== 'denied') {
-        const permission = await Notification.requestPermission();
-
-        if (permission === 'granted') {
-          showNotification("notifica");
-        }
-      }
-    }
-  };
-
-  const showNotification = body => {
-    const title = 'What PWA Can Do Today';
-
-    const payload = {
-      body
-    };
-
-    if ('showNotification' in registration) {
-      registration.showNotification(title, payload);
-    }
-    else {
-      new Notification(title, payload);
-    }
-  };
 
   const subscribe = async () => {
     // Check if push notifications are supported and allowed
@@ -49,9 +19,7 @@ function App() {
         if (!subscription) {
           alert('No subscription found, generating a new one');
           const response = await fetch(`${process.env.REACT_APP_API}/push/get-vapid-public-key`);
-          alert(`Response status: ${response.status}`);
           const vapidPublicKey = await response.text();
-          alert(`Vapid public key: ${vapidPublicKey}`);
           subscription = await serviceWorkerRegistration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: vapidPublicKey
@@ -85,7 +53,6 @@ function App() {
 
   return (
     <div>
-      <button onClick={sendNotification}>notify</button>
       <button onClick={subscribe}>Subcribe</button>
       PWA Push Notification
     </div>
