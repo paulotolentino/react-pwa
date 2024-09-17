0d35678730b8019b72835ecd993405ce49e786c7
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-09-17 12:08:07 -0300

    Add user gesture before subscription

--- src/App.js
+++ src/App.js
@@ -32,9 +32,59 @@ function App() {
     }
   };
 
+  const subscribe = async () => {
+    // Check if push notifications are supported and allowed
+    if (navigator.serviceWorker && window.PushManager && window.Notification) {
+      let subscription = null;
+      try {
+        subscription = await serviceWorkerRegistration.pushManager.getSubscription();
+      } catch (error) {
+        alert(`Error getting subscription: ${JSON.stringify(error)}`);
+        return;
+      }
+
+      try {
+        if (!subscription) {
+          alert('No subscription found, generating a new one');
+          const response = await fetch(`${process.env.REACT_APP_API}/push/get-vapid-public-key`);
+          alert(`Response status: ${response.status}`);
+          const vapidPublicKey = await response.text();
+          alert(`Vapid public key: ${vapidPublicKey}`);
+          subscription = await serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: vapidPublicKey
+          }).then(function (subscription) {
+            alert('Subscription generated');
+            return subscription;
+          }).catch(function (error) {
+            alert(`Error on subscribe method: ${error}`);
+            throw error;
+          });;
+        }
+      } catch (error) {
+        alert(`Error generating subscription: ${JSON.stringify(error)}`);
+        return;
+      }
+
+      try {
+
+        await fetch(`${process.env.REACT_APP_API}/push/subscribe`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(subscription)
+        });
+      } catch (error) {
+        alert(`Error sending subscription to server: ${JSON.stringify(error)}`);
+      }
+    }
+  }
+
   return (
     <div>
       <button onClick={sendNotification}>notify</button>
+      <button onClick={subscribe}>Subcribe</button>
       PWA Push Notification
     </div>
   );
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,50 +1,8 @@
 export function register() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(async (serviceWorkerRegistration) => {
+    navigator.serviceWorker.register('service-worker.js').then(() => {
       alert('Service worker registered');
-      let subscription = null;
-      try {
-        subscription = await serviceWorkerRegistration.pushManager.getSubscription();
-      } catch (error) {
-        alert(`Error getting subscription: ${JSON.stringify(error)}`);
-        return;
-      }
 
-      try {
-        if (!subscription) {
-          alert('No subscription found, generating a new one');
-          const response = await fetch(`${process.env.REACT_APP_API}/push/get-vapid-public-key`);
-          alert(`Response status: ${response.status}`);
-          const vapidPublicKey = await response.text();
-          alert(`Vapid public key: ${vapidPublicKey}`);
-          subscription = await serviceWorkerRegistration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: vapidPublicKey
-          }).then(function (subscription) {
-            alert('Subscription generated');
-            return subscription;
-          }).catch(function (error) {
-            alert(`Error on subscribe method: ${error}`);
-            throw error;
-          });;
-        }
-      } catch (error) {
-        alert(`Error generating subscription: ${JSON.stringify(error)}`);
-        return;
-      }
-
-      try {
-
-        await fetch(`${process.env.REACT_APP_API}/push/subscribe`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(subscription)
-        });
-      } catch (error) {
-        alert(`Error sending subscription to server: ${JSON.stringify(error)}`);
-      }
     }).catch((error) => {
       alert(`Error registering service worker: ${JSON.stringify(error)}`);
     });
